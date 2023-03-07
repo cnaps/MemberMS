96d696bc578765537bb678eba4922fdbbaf6c92b
cnaps <scant10@gmail.com>
2023-03-07 20:14:45 +0900

    init

---
+++ src/main/java/com/inflearn/memberms/MemberMsApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.memberms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberMsApplication.class, args);
+    }
+
+}
